b4d33924e80159e04455c828eb2c9feeb5f0f986
Michael <ccUser@cc-notebook-19.home>
2015-04-12 19:32:25 -0400

    Moving example to examples folder

---
+++ examples/example.py
@@ -0,0 +1,14 @@
+from playlistmaker import *
+
+block = """
+Half a league, half a league,
+Half a league onward,
+All in the valley of Death
+Rode the six hundred.
+"Forward, the Light Brigade!
+Charge for the guns!" he said:
+Into the valley of Death
+Rode the six hundred.
+"""
+
+print playlistmaker(block)
